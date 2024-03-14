75566f88c0f668f190157f9ee95a04dd90e7fd92
lequanghuy19204 <152720236+lequanghuy19204@users.noreply.github.com>
2024-03-14 07:51:28 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
-
-        Bài làm viết tại đây
-
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+                line = reader.readLine();
+            }
+            String content = stringBuilder.toString();
+            reader.close();
+            content = content.replaceAll("Nha Trang", "Vũng Tàu");
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write(content);
+            writer.close();
+            System.out.println("Thay thế và ghi nội dung vào file output.txt thành công!");
+        } catch (IOException e) {
+            System.out.println("Đã xảy ra lỗi khi thực hiện thao tác với file");
+        }
     }
 }
